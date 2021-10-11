f4b866d616182037106d553f78f461c81fa22852
Sookim18 <scstnghks@naver.com>
2021-10-11 13:18:41 +0900

    ✨ [Feat] : ProfileModel (개인정보 저장모델타입)추가

---
+++ iOS-drink-water/iOS-drink-water/ProfileModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileModel.swift
+//  iOS-drink-water
+//
+//  Created by sookim on 2021/10/11.
+//
+
+import Foundation
+
+struct ProfileModel {
+    
+    let nickname: String
+    let height: Double
+    let weight: Double
+    
+}
