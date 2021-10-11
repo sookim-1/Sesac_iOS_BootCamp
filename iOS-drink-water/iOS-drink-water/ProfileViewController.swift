90e0dae3ebca8836811828f43fc403d6be5a35c9
sookim-1 <scstnghks@gmail.com>
2021-10-11 14:34:16 +0900

    ✨ [Feat] : 메인화면 네비게이션바버튼 기능 구현(프로필화면전환, 새로고침)

--- iOS-drink-water/iOS-drink-water/ProfileViewController.swift
+++ iOS-drink-water/iOS-drink-water/ProfileViewController.swift
@@ -16,7 +16,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setNavigationBar()
     }
     
     private func setNavigationBar() {
