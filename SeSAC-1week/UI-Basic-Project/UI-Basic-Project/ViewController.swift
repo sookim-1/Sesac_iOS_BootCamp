ec1d93a8dd329f017d0e468ab28680cb54a70de5
sookim-1 <scstnghks@gmail.com>
2021-10-28 19:49:57 +0900

    [#2] 🎉 프로젝트 시작

---
+++ SeSAC-1week/UI-Basic-Project/UI-Basic-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI-Basic-Project
+//
+//  Created by sookim on 2021/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
