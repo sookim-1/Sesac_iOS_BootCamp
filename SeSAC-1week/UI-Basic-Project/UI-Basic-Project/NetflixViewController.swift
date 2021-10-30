f6ea2613f68ec449c2eeb5ead3e6d12153721117
sookim-1 <scstnghks@gmail.com>
2021-10-30 18:17:13 +0900

    [#3] ✨ [Feat] : 넷플릭스 재생버튼클릭시 이미지 랜덤으로 보여주는 기능구현

--- SeSAC-1week/UI-Basic-Project/UI-Basic-Project/NetflixViewController.swift
+++ SeSAC-1week/UI-Basic-Project/UI-Basic-Project/NetflixViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class NetflixViewController: UIViewController {
 
+    @IBOutlet var mainImageView: UIImageView!
     @IBOutlet var subImageView1: UIImageView!
     @IBOutlet var subImageView2: UIImageView!
     @IBOutlet var subImageView3: UIImageView!
     
+    var randomNum: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,5 +30,25 @@ class NetflixViewController: UIViewController {
         imageView.layer.borderWidth = 1
         imageView.layer.borderColor = UIColor.systemRed.cgColor
     }
+    
+    @IBAction func clickRandomButton(_ sender: UIButton) {
+        randomNum.removeAll()
+        randomNum.append(Int.random(in: 1...10))
+
+        // 중복제거
+        while randomNum.count != 4 {
+            let tempNumber = Int.random(in: 1...10)
+            
+            if randomNum.contains(tempNumber) == false {
+                randomNum.append(tempNumber)
+            }
+        }
+        
+        mainImageView.image = UIImage(named: "poster\(randomNum[0])")
+        subImageView1.image = UIImage(named: "poster\(randomNum[1])")
+        subImageView2.image = UIImage(named: "poster\(randomNum[2])")
+        subImageView3.image = UIImage(named: "poster\(randomNum[3])")
+    }
+
 }
 
