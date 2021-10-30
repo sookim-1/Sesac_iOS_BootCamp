7836d2b266acfb99bb5bd5bef400c18a4186990b
sookim-1 <scstnghks@gmail.com>
2021-10-30 22:27:47 +0900

    [#5] 💄 [UI] : 전광판앱 - IBOutlet, IBAction 연결

--- SeSAC-1week/LEDBoard/LEDBoard/BoardViewController.swift
+++ SeSAC-1week/LEDBoard/LEDBoard/BoardViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class BoardViewController: UIViewController {
 
+    @IBOutlet var BoardView: UIView!
+    @IBOutlet var ResultLabel: UILabel!
+    @IBOutlet var SendButton: UIButton!
+    @IBOutlet var TextColorButton: UIButton!
+    @IBOutlet var UserTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sendToLabel(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeColor(_ sender: UIButton) {
+    }
+    
 }
 
