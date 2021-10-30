39ecb1d1ac537d0f1eebe288b76863f55c9f5a0a
sookim-1 <scstnghks@gmail.com>
2021-10-30 19:19:02 +0900

    [#4] 💄 [UI] : 넷플릭스 회원가입 화면 기본요소 추가

---
+++ SeSAC-1week/UI-Basic-Project/UI-Basic-Project/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  UI-Basic-Project
+//
+//  Created by sookim on 2021/10/30.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
