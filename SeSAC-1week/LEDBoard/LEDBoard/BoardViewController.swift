6e70a330b1e3630e69339a234b645076a6caee39
sookim-1 <scstnghks@gmail.com>
2021-10-30 22:43:44 +0900

    [#5] 💄 [UI] : 전광판앱 - UI요소들 속성 지정

--- SeSAC-1week/LEDBoard/LEDBoard/BoardViewController.swift
+++ SeSAC-1week/LEDBoard/LEDBoard/BoardViewController.swift
@@ -17,7 +17,8 @@ class BoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configure()
     }
 
     @IBAction func sendToLabel(_ sender: UIButton) {
@@ -26,5 +27,21 @@ class BoardViewController: UIViewController {
     @IBAction func changeColor(_ sender: UIButton) {
     }
     
+    func configure() {
+        BoardView.layer.cornerRadius = 10
+        
+        UserTextField.layer.borderWidth = 0
+        UserTextField.borderStyle = .none
+        
+        SendButton.layer.borderWidth = 5
+        SendButton.layer.borderColor = UIColor.label.cgColor
+        SendButton.layer.cornerRadius = 10
+        SendButton.tintColor = .label
+        
+        TextColorButton.layer.borderWidth = 5
+        TextColorButton.layer.borderColor = UIColor.label.cgColor
+        TextColorButton.layer.cornerRadius = 10
+        TextColorButton.tintColor = .systemRed
+    }
 }
 
