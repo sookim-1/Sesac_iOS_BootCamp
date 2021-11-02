b976ad033d2a2938ebacbd4c9ceba6aea10562f0
sookim-1 <scstnghks@gmail.com>
2021-11-03 00:59:40 +0900

    [#24] 🎉 프로젝트 시작

---
+++ SeSAC-6week/Diary-Project/Diary-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary-Project
+//
+//  Created by sookim on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
