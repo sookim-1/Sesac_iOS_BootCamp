35375573800037e63c53c99d31881d5f41c5ede0
sookim-1 <scstnghks@gmail.com>
2021-11-05 16:33:22 +0900

    [#9] ✨ [Feat] : 목표섭취량(%단위) 구현

--- SeSAC-2week/drinkWaterProject/drinkWaterProject/DrinkWaterViewController.swift
+++ SeSAC-2week/drinkWaterProject/drinkWaterProject/DrinkWaterViewController.swift
@@ -11,26 +11,31 @@ class DrinkWaterViewController: UIViewController {
     @IBOutlet var currentWaterAmountTextField: UITextField!
     @IBOutlet var totalWaterAmountLabel: UILabel!
     @IBOutlet var recommendWaterAmountLabel: UILabel!
+    @IBOutlet var goalWaterAmountLabel: UILabel!
     
     var totalWaterAmount: Int = UserDefaults.standard.integer(forKey: "totalWaterAmount")
-
+    var recommendWaterAmount: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         totalWaterAmountLabel.text = String(totalWaterAmount)
         getProfileData()
+        refreshGoalWaterAmount(totalWaterAmount, recommendWaterAmount)
     }
 
     private func getProfileData() {
         guard let data = UserDefaults.standard.value(forKey: "profileModel") as? Data else { return }
         let profileModel = try? PropertyListDecoder().decode(ProfileModel.self, from: data)
-        recommendWaterAmountLabel.text = "\(profileModel?.recommendWaterAmount())"
+        recommendWaterAmount = profileModel?.recommendWaterAmount()
+        recommendWaterAmountLabel.text = "\(recommendWaterAmount)"
     }
 
     private func setNavigationBar() {
         self.title = "물 마시기"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshTotalWaterAmount))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.circle"), style: .plain, target: self, action: #selector(presentProfileViewController))
+        refreshGoalWaterAmount(totalWaterAmount, recommendWaterAmount)
     }
 
     @IBAction func drinkWater(_ sender: UIButton) {
@@ -52,6 +57,16 @@ class DrinkWaterViewController: UIViewController {
         totalWaterAmount = 0
         UserDefaults.standard.set(totalWaterAmount, forKey: "totalWaterAmount")
         totalWaterAmountLabel.text = String(totalWaterAmount)
+        
+        refreshGoalWaterAmount(totalWaterAmount, recommendWaterAmount)
+    }
+    
+    private func refreshGoalWaterAmount(_ totalWaterAmount: Int, _ recommendWaterAmount: Double?) {
+        guard let recommendWaterAmount = recommendWaterAmount else { return }
+
+        let literRecommendWaterAmount = recommendWaterAmount * 1000
+        let goalWaterAmount = Double(totalWaterAmount) / literRecommendWaterAmount
+        goalWaterAmountLabel.text = "목표의 \(Int(round(goalWaterAmount * 100)))%"
     }
 }
 
