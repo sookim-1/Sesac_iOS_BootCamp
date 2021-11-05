a0f3351e4482efcd84f24352b71f935a2d5e4101
sookim-1 <scstnghks@gmail.com>
2021-11-05 16:22:08 +0900

    [#9] ✨ [Feat] : ProfileModel (개인정보 저장모델타입)추가

---
+++ SeSAC-2week/drinkWaterProject/drinkWaterProject/ProfileModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileModel.swift
+//  drinkWaterProject
+//
+//  Created by sookim on 2021/11/05.
+//
+
+import Foundation
+
+struct ProfileModel {
+
+    let nickname: String
+    let height: Double
+    let weight: Double
+
+}
