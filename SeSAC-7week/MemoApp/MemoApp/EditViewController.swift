70e04c728f2c3fedfc31cb07cec81084a8ef53de
sookim-1 <scstnghks@gmail.com>
2021-11-10 04:14:06 +0900

    [#28] 💄 [UI] : 메모 작성/수정화면 추가

---
+++ SeSAC-7week/MemoApp/MemoApp/EditViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditViewController.swift
+//  MemoApp
+//
+//  Created by sookim on 2021/11/10.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var memoTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
