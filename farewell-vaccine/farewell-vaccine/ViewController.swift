6b1033f96e6f4cbd97c4ae923aedceeb3aef9ef7
Sookim18 <scstnghks@naver.com>
2021-11-18 15:37:18 +0900

    [#1] 🎉 프로젝트 시작

---
+++ farewell-vaccine/farewell-vaccine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  farewell-vaccine
+//
+//  Created by sookim on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
