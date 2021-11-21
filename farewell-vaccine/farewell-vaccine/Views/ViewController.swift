e6b8c3f7edd7fc9dafa7a9b6ca5a3b7be12d4b4c
Sookim18 <scstnghks@naver.com>
2021-11-21 23:52:50 +0900

    [#2] 💄 [UI] : 메인화면 - 상대프로필 지정화면 구현

--- farewell-vaccine/farewell-vaccine/Views/ViewController.swift
+++ farewell-vaccine/farewell-vaccine/Views/ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var settingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureSettingBtn()
+        configureNavigationBar()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -23,6 +30,22 @@ class ViewController: UIViewController {
         
     }
     
+    func configureNavigationBar() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for:.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+
+//        let barAppearance = UINavigationBarAppearance()
+//
+//        barAppearance.backgroundColor = .white
+//        self.navigationItem.standardAppearance = barAppearance
+//        self.navigationItem.scrollEdgeAppearance = barAppearance
+        
+        self.title = "이별백신"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "line.horizontal.3"), style: .plain, target: self, action: #selector(presentSideMenu))
+        self.navigationController?.navigationBar.tintColor = .systemBlue
+
+    }
+    
     func presentOnboardingVC() {
         guard let onboardingVC = UIStoryboard(name: "Onboarding", bundle: nil).instantiateViewController(withIdentifier: "OnboardingVC") as? OnboardingVC else { return }
         
@@ -32,7 +55,14 @@ class ViewController: UIViewController {
         self.present(onboardingVC, animated: true)
     }
     
-    @IBAction func presentSideMenu(_ sender: UIButton) {
+    func configureSettingBtn() {
+        settingButton.backgroundColor = UIColor(red: 254/255, green: 107/255, blue: 160/255, alpha: 1)
+        settingButton.setTitleColor(.white, for: .normal)
+        settingButton.setTitle("지정하기", for: .normal)
+        settingButton.layer.cornerRadius = 10
+    }
+            
+    @objc func presentSideMenu() {
         guard let sideMenuNC = UIStoryboard(name: "SideMenu", bundle: nil).instantiateViewController(withIdentifier: "SideMenuNC") as? SideMenuNC else { return }
         
         self.present(sideMenuNC, animated: true)
