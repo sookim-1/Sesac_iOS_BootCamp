d56c4d0a13f71aa8a4ca187b7f272e1e50c9b0db
sookim-1 <scstnghks@gmail.com>
2021-11-23 19:41:24 +0900

    [#2] 💄 [UI] : 소개화면 추가

---
+++ farewell-vaccine/farewell-vaccine/Views/IntroduceVC.swift
@@ -0,0 +1,19 @@
+//
+//  IntroduceVC.swift
+//  farewell-vaccine
+//
+//  Created by sookim on 2021/11/23.
+//
+
+import UIKit
+
+class IntroduceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
