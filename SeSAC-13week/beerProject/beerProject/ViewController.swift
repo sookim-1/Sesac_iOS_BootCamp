0f3cab562177095c8ec67ebc244cea5e3a4fc2f2
sookim-1 <scstnghks@gmail.com>
2021-12-21 07:03:15 +0900

    [#30] 🎉 프로젝트 시작

---
+++ SeSAC-13week/beerProject/beerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beerProject
+//
+//  Created by sookim on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
