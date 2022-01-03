ca2a16ba0d5b9b3cf362d97736db885744e2675d
sookim-1 <scstnghks@gmail.com>
2022-01-03 17:45:08 +0900

    🎨 [Style] : SwiftLint적용

--- SeSAC-Blog/SeSAC-Blog/SceneDelegate.swift
+++ SeSAC-Blog/SeSAC-Blog/SceneDelegate.swift
@@ -11,12 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window  = UIWindow(windowScene: windowScene)
         let mainVC = ViewController()
-        
+
         window?.rootViewController = mainVC
         window?.makeKeyAndVisible()
     }
@@ -49,6 +48,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
