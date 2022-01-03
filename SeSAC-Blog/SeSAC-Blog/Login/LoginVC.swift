ed8837e5278bfc4eeab079f4ad77a75486452ca0
Sookim18 <scstnghks@naver.com>
2022-01-04 02:48:33 +0900

    💄 [UI] : 로그인화면 추가

--- SeSAC-Blog/SeSAC-Blog/Login/LoginVC.swift
+++ SeSAC-Blog/SeSAC-Blog/Login/LoginVC.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    let mainView = LoginView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        title = "새싹농장 로그인하기"
     }
-
+    
 }
---
+++ SeSAC-Blog/SeSAC-Blog/Login/LoginView.swift
@@ -0,0 +1,56 @@
+//
+//  LoginView.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/04.
+//
+
+import UIKit
+import SnapKit
+
+class LoginView: UIView, ViewRepresentable {
+
+    let emailTextField = BlogBasicTextField(placeholderText: "이메일 주소")
+    let passwordTextField = BlogBasicTextField(placeholderText: "비밀번호")
+    let loginButton = BlogBasicButton(backgroundColor: .systemGray4, title: "로그인")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginButton)
+    }
+    
+    func setupConstraints() {
+        emailTextField.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(20)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.top.equalTo(emailTextField.snp.bottom).offset(20)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+        }
+    }
+}
