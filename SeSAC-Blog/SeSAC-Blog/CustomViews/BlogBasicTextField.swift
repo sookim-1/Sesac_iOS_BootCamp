2f713bcd1246c97144b198b2399a1b0fbed81963
sookim-1 <scstnghks@gmail.com>
2022-01-04 00:47:27 +0900

    💄 [UI] : 커스텀버튼, 커스텀텍스트필드 추가

---
+++ SeSAC-Blog/SeSAC-Blog/CustomViews/BlogBasicTextField.swift
@@ -0,0 +1,47 @@
+//
+//  BlogBasicTextField.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/04.
+//
+
+import UIKit
+
+class BlogBasicTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(placeholderText: String) {
+        super.init(frame: .zero)
+        
+        placeholder = placeholderText
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        returnKeyType = .go
+    }
+
+}
