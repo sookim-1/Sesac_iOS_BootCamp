7243eaa752b0e0cf03b74d2f39659faa13261166
sookim-1 <scstnghks@gmail.com>
2022-01-04 02:00:26 +0900

    ✨ [Feat] : 홈화면 버튼 액션 추가 - 화면전환

--- SeSAC-Blog/SeSAC-Blog/Home/HomeVC.swift
+++ SeSAC-Blog/SeSAC-Blog/Home/HomeVC.swift
@@ -18,6 +18,19 @@ class HomeVC: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        configure()
     }
 
+    func configure() {
+        mainView.loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        mainView.signButton.addTarget(self, action: #selector(signButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonClicked() {
+        self.navigationController?.pushViewController(LoginVC(), animated: true)
+    }
+    
+    @objc func signButtonClicked() {
+        self.navigationController?.pushViewController(SignUpVC(), animated: true)
+    }
 }
---
+++ SeSAC-Blog/SeSAC-Blog/Login/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/04.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+}
