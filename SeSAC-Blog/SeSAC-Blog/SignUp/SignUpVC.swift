d7b004128b776e75bd042e0c7384a666f0761506
Sookim18 <scstnghks@naver.com>
2022-01-04 02:00:26 +0900

    ✨ [Feat] : 홈화면 버튼 액션 추가 - 화면전환

---
+++ SeSAC-Blog/SeSAC-Blog/SignUp/SignUpVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpVC.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/04.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+    
+}
