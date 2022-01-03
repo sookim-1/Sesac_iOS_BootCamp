13f5a1c3a53224bb56a4bd05e3349fc3cccb508c
sookim-1 <scstnghks@gmail.com>
2022-01-04 01:50:20 +0900

    💄 [UI] : 메인 홈화면 추가 - 공통뷰 프로토콜 추가 - 오토레이아웃 적용 - 공통 뷰 프로토콜 작성(ViewRepresentable)

---
+++ SeSAC-Blog/SeSAC-Blog/Protocols/ViewRepresentable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewRepresentable.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/04.
+//
+
+import Foundation
+
+protocol ViewRepresentable {
+    func configure()
+    func setupConstraints()
+}
