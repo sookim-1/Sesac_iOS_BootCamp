37a27cde31b2a37c8f7410ccb06755e62e9494c7
Sookim18 <scstnghks@naver.com>
2022-01-11 03:14:53 +0900

    💄 [UI] : 게시글 화면 구현

---
+++ SeSAC-Blog/SeSAC-Blog/Post/PostListVC.swift
@@ -0,0 +1,51 @@
+//
+//  TestVC.swift
+//  SeSAC-Blog
+//
+//  Created by sookim on 2022/01/11.
+//
+
+import UIKit
+import SnapKit
+
+class PostListVC: UIViewController {
+
+    let mainView = PostListView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
+        mainView.actionButton.addTarget(self, action: #selector(presentPostEditVC), for: .touchUpInside)
+    }
+
+    @objc func presentPostEditVC() {
+        print("edit")
+    }
+    
+}
+
+extension PostListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = mainView.tableView.dequeueReusableCell(withIdentifier: PostListTableViewCell.identifier, for: indexPath) as? PostListTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.nickNameLabel.text = "미묘한도사"
+        cell.bodyLabel.text = "드라이브가실분 심심해영"
+        cell.dateLabel.text = "12/04"
+        
+        cell.commentButton.setTitle("댓글 4", for: .normal)
+        return cell
+    }
+    
+}
