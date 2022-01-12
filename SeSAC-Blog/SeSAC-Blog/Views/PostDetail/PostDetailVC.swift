49a635f67066d99c2f0fa521dbd89ca841812e33
Sookim18 <scstnghks@naver.com>
2022-01-13 03:54:40 +0900

    ✨ [Feat] : 게시글 정보화면 구현

--- SeSAC-Blog/SeSAC-Blog/Views/PostDetail/PostDetailVC.swift
+++ SeSAC-Blog/SeSAC-Blog/Views/PostDetail/PostDetailVC.swift
@@ -30,6 +30,19 @@ class PostDetailVC: BaseVC {
         mainView.tableView.delegate = self
         mainView.tableView.dataSource = self
         configure()
+
+        bindingTextField(viewModel.commentText, self.mainView.commentTextField)
+        mainView.commentTextField.addTarget(self, action: #selector(commentTextFieldDidChange(_:)), for: .editingChanged)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     func configure() {
@@ -38,6 +51,7 @@ class PostDetailVC: BaseVC {
         self.mainView.postView.nicknameLabel.text = postData.user.username
         self.mainView.postView.bodyLabel.text = postData.text
         self.mainView.postView.dateLabel.text = self.dateFormatter.string(from: postData.updatedAt.toDate()!)
+        self.mainView.commentTextField.delegate = self
 
         viewModel.getCommentData(id: postData.id) { result in
             switch result {
@@ -58,6 +72,26 @@ class PostDetailVC: BaseVC {
         }
     }
 
+    @objc func commentTextFieldDidChange(_ textfield: UITextField) {
+        viewModel.commentText.value = textfield.text ?? ""
+    }
+
+    @objc func keyboardWillShow(_ noti: NSNotification) {
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y -= keyboardHeight
+        }
+    }
+
+    @objc func keyboardWillHide(_ noti: NSNotification) {
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y += keyboardHeight
+        }
+    }
+
 }
 
 extension PostDetailVC: UITableViewDelegate, UITableViewDataSource {
@@ -74,6 +108,34 @@ extension PostDetailVC: UITableViewDelegate, UITableViewDataSource {
         cell.bodyLabel.text = commentData[indexPath.row].comment
 
         return cell
+    }
+
+}
+
+extension PostDetailVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        print(viewModel.commentText.value, postData?.id ?? 0)
+        viewModel.postComment(model: CommentModel(comment: viewModel.commentText.value, post: postData?.id ?? 0)) { result in
+            switch result {
+            case .success(_):
+                print("success")
+
+                self.view.endEditing(true)
+            case .failure(let error):
+                if error.errorTag == 1 {
+                    DispatchQueue.main.async {
+                        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+                        windowScene.windows.first?.rootViewController = UINavigationController(rootViewController: LoginVC())
+                        windowScene.windows.first?.makeKeyAndVisible()
+                    }
+                }
+                print(error.localizedDescription)
+                self.presentErrorAlertOnMainThread(title: "댓글 작성 오류", message: "댓글을 작성할 수 없습니다.", buttonTitle: "확인")
+                self.view.endEditing(true)
+            }
+        }
+        return true
     }
 
 }
