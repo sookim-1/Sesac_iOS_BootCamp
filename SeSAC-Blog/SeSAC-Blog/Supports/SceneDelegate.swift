2b3a8a97b5bc9972206d19d0bb4d00a635fffd8a
Sookim18 <scstnghks@naver.com>
2022-01-12 18:50:44 +0900

    ✨ [Feat] : 로그인여부에 따라 초기화면 분리

--- SeSAC-Blog/SeSAC-Blog/Supports/SceneDelegate.swift
+++ SeSAC-Blog/SeSAC-Blog/Supports/SceneDelegate.swift
@@ -14,7 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window  = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: HomeVC())
+        if let _ = UserDefaults.standard.string(forKey: "token") {
+            window?.rootViewController = UINavigationController(rootViewController: PostListVC())
+        } else {
+            window?.rootViewController = UINavigationController(rootViewController: HomeVC())
+        }
         window?.makeKeyAndVisible()
     }
 
