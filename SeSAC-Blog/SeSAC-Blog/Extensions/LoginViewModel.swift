c885a53a7024ce50ff361f9ac1c3ffd14dcbe0a5
sookim-1 <scstnghks@gmail.com>
2022-01-16 02:07:30 +0900

    🐛 [Fix] : 로그인 네트워크 통신 에러처리 해결

--- SeSAC-Blog/SeSAC-Blog/Extensions/LoginViewModel.swift
+++ SeSAC-Blog/SeSAC-Blog/Extensions/LoginViewModel.swift
@@ -19,6 +19,36 @@ final class LoginViewModel {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = try? JSONEncoder().encode(model)
         URLSession.request(endpoint: request, completion: completion)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.customError(errorMessage: "네트워크에러")))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.customError(errorMessage: "데이터통신에러")))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(.customError(errorMessage: "response응답 에러")))
+                return
+            }
+
+            guard response.statusCode == 200 else {
+                completion(.failure(.customError(errorMessage: "response상태코드 에러:\(response.statusCode)")))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let userData = try decoder.decode(ResponseLoginModel.self, from: data)
+                completion(.success(userData))
+            } catch {
+                completion(.failure(.customError(errorMessage: "디코딩 에러")))
+            }
+        }
     }
 
     func checkEditAllTextField(onComplete: @escaping (Bool) -> Void) {
