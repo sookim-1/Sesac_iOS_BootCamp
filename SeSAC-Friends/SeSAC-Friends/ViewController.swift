228b96522415f5c869f0e2bc74599ecb05f66749
sookim-1 <scstnghks@gmail.com>
2022-01-17 11:52:29 +0900

    🎉 프로젝트 시작 - 최소타겟버전 13.0 - 세로모드만 지원 - 코드 UI설정

---
+++ SeSAC-Friends/SeSAC-Friends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeSAC-Friends
+//
+//  Created by sookim on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
