b6af1c9b926db95f1874a084240478ef2e6b2e47
sookim-1 <scstnghks@gmail.com>
2022-01-20 06:57:29 +0900

    💄 [UI] : 온보딩 화면구현

--- SeSAC-Friends/SeSAC-Friends/SceneDelegate.swift
+++ SeSAC-Friends/SeSAC-Friends/SceneDelegate.swift
@@ -15,7 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        let firstLaunch = FirstLaunch(userDefaults: .standard, key: "firstLaunchKey")
+        firstLaunch.isFirstLaunch ? (window?.rootViewController = OnboardingVC()) : (window?.rootViewController = UINavigationController(rootViewController: ViewController()))
+        window?.rootViewController = OnboardingVC()
         window?.makeKeyAndVisible()
     }
 
