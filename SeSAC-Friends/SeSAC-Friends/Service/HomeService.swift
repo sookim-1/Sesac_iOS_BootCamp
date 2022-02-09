60722fdc670dd435dae2b4f892f76fccf4199834
sookim-1 <scstnghks@gmail.com>
2022-02-10 06:27:43 +0900

    🔥 [Chore] : 불필요한 파일 및 코드 제거

--- SeSAC-Friends/SeSAC-Friends/Service/HomeService.swift
+++ SeSAC-Friends/SeSAC-Friends/Service/HomeService.swift
@@ -14,22 +14,12 @@ struct HomeService {
     private let headers: HTTPHeaders = [
         "idtoken": "\(UserDefaults.idToken)"
     ]
-//    
-    func getSesacUser(completion: @escaping (DataResponse<UserInfoModel, AFError>) -> Void) {
-        AF.request(Endpoint.user.url, method: .get, parameters: [:], headers: headers)
-            .validate(contentType: ["application/json"])
-            .responseDecodable(of: UserInfoModel.self, completionHandler: completion)
-    }
-    
+
     func postOnQueue(model: OnQueue, completion: @escaping (DataResponse<Post, AFError>) -> Void) {
         
         AF.request(Endpoint.onqueue.url, method: .post, parameters: model, encoder: JSONParameterEncoder.default, headers: headers)
             .validate(contentType: ["application/json"])
             .responseDecodable(of: Post.self, completionHandler: completion)
     }
-//
-//    func postWithDraw(completion: @escaping (AFDataResponse<String>) -> Void) {
-//        AF.request(Endpoint.withdraw.url, method: .post, parameters: [:], headers: headers)
-//            .responseString(completionHandler: completion)
-//    }
+    
 }
